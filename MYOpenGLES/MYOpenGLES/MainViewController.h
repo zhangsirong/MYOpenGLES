cd257a4a85d963db429cec2508e37acf90ebb45b
hp <admin@admindeimac-3.local>
2016-11-29 12:08:30 +0800

    纹理的取样模式(线性，邻近插值)和纹理的环绕方式(重复，边缘拉伸)

--- MYOpenGLES/MYOpenGLES/MainViewController.h
+++ MYOpenGLES/MYOpenGLES/MainViewController.h
@@ -16,5 +16,9 @@
 
 @property (strong, nonatomic) GLKBaseEffect *baseEffect;
 @property (strong, nonatomic) MYGLKVertexAttribArrayBuffer *vertexBuffer;
+@property (nonatomic) BOOL shouldUseLinearFilter;
+@property (nonatomic) BOOL shouldAnimate;
+@property (nonatomic) BOOL shouldRepeatTexture;
+@property (nonatomic) GLfloat sCoordinateOffset;
 
 @end
--- MYOpenGLES/MYOpenGLES/MainViewController.m
+++ MYOpenGLES/MYOpenGLES/MainViewController.m
@@ -9,7 +9,26 @@
 #import "MainViewController.h"
 #import "MYEAGLContext.h"
 #import "MYGLKVertexAttribArrayBuffer.h"
-#import "MYGLKTextureLoader.h"
+
+
+@interface GLKEffectPropertyTexture (MYGLKAdditions)
+
+- (void)glkSetParameter:(GLenum)parameterID
+                  value:(GLint)value;
+
+@end
+
+@implementation GLKEffectPropertyTexture (MYGLKAdditions)
+
+- (void)glkSetParameter:(GLenum)parameterID
+                  value:(GLint)value;
+{
+    glBindTexture(self.target, self.name);
+    glTexParameteri(self.target, parameterID, value);
+}
+
+@end
+
 
 @interface MainViewController ()
 
@@ -18,6 +37,10 @@
 @implementation MainViewController
 @synthesize baseEffect;
 @synthesize vertexBuffer;
+@synthesize shouldUseLinearFilter;
+@synthesize shouldAnimate;
+@synthesize shouldRepeatTexture;
+@synthesize sCoordinateOffset;
 
 
 typedef struct {
@@ -26,36 +49,69 @@ typedef struct {
 }
 SceneVertex;
 
-static const SceneVertex vertices[] =
+static SceneVertex vertices[] =
 {
     {{-0.5f, -0.5f, 0.0}, {0.0f,0.0f}},          // 左下角
     {{ 0.5f, -0.5f, 0.0}, {1.0f,0.0f}},          // 右下角
     {{-0.5f,  0.5f, 0.0}, {0.0f,1.0f}}           // 左上角
 };
 
+static const SceneVertex defaultVertices[] =
+{
+    {{-0.5f, -0.5f, 0.0f}, {0.0f, 0.0f}},
+    {{ 0.5f, -0.5f, 0.0f}, {1.0f, 0.0f}},
+    {{-0.5f,  0.5f, 0.0f}, {0.0f, 1.0f}},
+};
+
+static GLKVector3 movementVectors[3] =
+{
+    {-0.02f,  -0.01f, 0.0f},
+    {0.01f,  -0.005f, 0.0f},
+    {-0.01f,   0.01f, 0.0f},
+};
+
+
+#pragma mark - ViewLifeCycle
+
 -(void)viewDidLoad
 {
     [super viewDidLoad];
+    self.preferredFramesPerSecond = 60;
+
+    [self configSubViews];
     GLKView *view = (GLKView *)self.view;
-    
     view.context = [[MYEAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2];
     [EAGLContext setCurrentContext:view.context];
     self.baseEffect = [[GLKBaseEffect alloc] init];
     self.baseEffect.useConstantColor = GL_TRUE;
     self.baseEffect.constantColor = GLKVector4Make(1.0f, 1.0f, 1.0f, 1.0f);                     //着色语言 三角形颜色
     
-    ((MYEAGLContext *)view.context).clearColor = GLKVector4Make(0.0f, 0.0f, 1.0f, 1.0f);      //背景
+    ((MYEAGLContext *)view.context).clearColor = GLKVector4Make(0.0f, 1.0, 0.0f, 0.3f);      //背景
     
     self.vertexBuffer = [[MYGLKVertexAttribArrayBuffer alloc] initWithAttribStride:sizeof(SceneVertex)
                                                                   numberOfVertices:sizeof(vertices) / sizeof(SceneVertex)
                                                                              bytes:vertices
                                                                              usage:GL_STATIC_DRAW];
     
-    CGImageRef imageRef = [UIImage imageNamed:@"leaves.gif"].CGImage;
-    MYGLKTextureInfo *textureInfo = [MYGLKTextureLoader textureWithCGImage:imageRef options:nil error:NULL];
+    CGImageRef imageRef = [UIImage imageNamed:@"grid.png"].CGImage;
+    GLKTextureInfo *textureInfo = [GLKTextureLoader textureWithCGImage:imageRef options:nil error:NULL];
     self.baseEffect.texture2d0.name = textureInfo.name;
     self.baseEffect.texture2d0.target = textureInfo.target;
 }
+
+-(void)viewDidUnload
+{
+    [super viewDidUnload];
+    
+    GLKView *view = (GLKView *)self.view;
+    [MYEAGLContext setCurrentContext:view.context];
+    self.vertexBuffer = nil;
+    view.context = nil;
+    [EAGLContext setCurrentContext:nil];
+}
+
+
+#pragma mark - GLKViewDelegate
 
 -(void)glkView:(GLKView *)view drawInRect:(CGRect)rect
 {
@@ -76,14 +132,115 @@ static const SceneVertex vertices[] =
                         numberOfVertices:3];
 }
 
--(void)viewDidUnload
+
+#pragma mark - 刷新每帧
+
+-(void)update
 {
-    [super viewDidUnload];
-    
-    GLKView *view = (GLKView *)self.view;
-    [MYEAGLContext setCurrentContext:view.context];
-    self.vertexBuffer = nil;
-    view.context = nil;
-    [EAGLContext setCurrentContext:nil];
+    [self updateAnimatedVertexPositions];
+    [self updateTextureParameters];
+    [vertexBuffer reinitWithAttribStride:sizeof(SceneVertex)
+                        numberOfVertices:sizeof(vertices)/sizeof(SceneVertex)
+                                   bytes:vertices];
 }
+
+-(void)updateTextureParameters
+{
+    [self.baseEffect.texture2d0 glkSetParameter:GL_TEXTURE_WRAP_S value:self.shouldRepeatTexture ? GL_REPEAT : GL_CLAMP_TO_EDGE];
+    [self.baseEffect.texture2d0 glkSetParameter:GL_TEXTURE_MAG_FILTER value:self.shouldUseLinearFilter ? GL_LINEAR : GL_NEAREST];
+}
+
+-(void)updateAnimatedVertexPositions
+{
+    if (shouldAnimate) {
+        for (int i = 0; i < 3; i++) {
+            vertices[i].positionCoords.x += movementVectors[i].x;
+            if(vertices[i].positionCoords.x >= 1.0f || vertices[i].positionCoords.x <= -1.0f) {
+                movementVectors[i].x = -movementVectors[i].x;
+            }
+            
+            vertices[i].positionCoords.y += movementVectors[i].y;
+            if(vertices[i].positionCoords.y >= 1.0f || vertices[i].positionCoords.y <= -1.0f) {
+                movementVectors[i].y = -movementVectors[i].y;
+            }
+            
+            vertices[i].positionCoords.z += movementVectors[i].z;
+            if(vertices[i].positionCoords.z >= 1.0f || vertices[i].positionCoords.z <= -1.0f) {
+                movementVectors[i].z = -movementVectors[i].z;
+            }
+        }
+    }else{
+        for (int i = 0; i < 3; i++) {
+            vertices[i].positionCoords.x = defaultVertices[i].positionCoords.x;
+            vertices[i].positionCoords.y = defaultVertices[i].positionCoords.y;
+            vertices[i].positionCoords.z = defaultVertices[i].positionCoords.z;
+        }
+    }
+    
+    for(int i = 0; i < 3; i++){
+        vertices[i].textureCoords.s = (defaultVertices[i].textureCoords.s + sCoordinateOffset);
+    }
+}
+
+
+#pragma mark - Private
+
+-(void)configSubViews
+{
+    CGFloat screenWidth = [UIScreen mainScreen].bounds.size.width;
+    CGFloat screenHeight = [UIScreen mainScreen].bounds.size.height;
+    
+    UISwitch *shouldUseLinearFilterSwitch =  [[UISwitch alloc] initWithFrame:CGRectMake(20, 20, 100, 50)];
+    [shouldUseLinearFilterSwitch addTarget:self action:@selector(takeShouldUseLinearFilterFrom:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:shouldUseLinearFilterSwitch];
+    UILabel *linearFilterlabel = [[UILabel alloc] initWithFrame:CGRectMake(100, 20, 100, 50)];
+    linearFilterlabel.text = @"LinearFilter";
+    [self.view addSubview:linearFilterlabel];
+    
+    UISwitch *shouldAnimateSwitch =  [[UISwitch alloc] initWithFrame:CGRectMake(20, screenHeight * 0.5 - 60, 100, 50)];
+    [shouldAnimateSwitch addTarget:self action:@selector(takeShouldAnimateFrom:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:shouldAnimateSwitch];
+    UILabel *animateLabel = [[UILabel alloc] initWithFrame:CGRectMake(100, screenHeight * 0.5 - 60, 100, 50)];
+    animateLabel.text = @"Animate";
+    [self.view addSubview:animateLabel];
+    
+    UISwitch *shouldRepeatTextureSwitch =  [[UISwitch alloc] initWithFrame:CGRectMake(20, screenHeight - 50, 100, 50)];
+    [shouldRepeatTextureSwitch addTarget:self action:@selector(takeShouldRepeatTextureFrom:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:shouldRepeatTextureSwitch];
+    UILabel *repeatTexturelabel = [[UILabel alloc] initWithFrame:CGRectMake(100, screenHeight - 50, 100, 50)];
+    repeatTexturelabel.text = @"RepeatTexture";
+    [self.view addSubview:repeatTexturelabel];
+    
+    UISlider *sCoordinateOffsetSlider = [[UISlider alloc] initWithFrame:CGRectMake(20, screenHeight * 0.5, screenWidth - 40, 50)];
+    sCoordinateOffsetSlider.minimumValue = -1.0f;
+    sCoordinateOffsetSlider.maximumValue = 1.0f;
+    sCoordinateOffsetSlider.value = 0.0f;
+    [sCoordinateOffsetSlider addTarget:self action:@selector(takeSCoordinateOffsetFrom:) forControlEvents:UIControlEventValueChanged];
+    [self.view addSubview:sCoordinateOffsetSlider];
+}
+
+
+#pragma mark - Event
+
+-(void)takeShouldUseLinearFilterFrom:(UISwitch *)sender
+{
+    self.shouldUseLinearFilter = [sender isOn];
+}
+
+-(void)takeShouldAnimateFrom:(UISwitch *)sender
+{
+    self.shouldAnimate = [sender isOn];
+}
+
+-(void)takeShouldRepeatTextureFrom:(UISwitch *)sender
+{
+    self.shouldRepeatTexture = [sender isOn];
+}
+
+-(void)takeSCoordinateOffsetFrom:(UISlider *)sender
+{
+    self.sCoordinateOffset = [sender value];
+}
+
+
 @end
